94c743fb6607b7f32ac99210b09af394b773b6d0
zaracama <cucigudang576@gmail.com>
2024-04-05 17:24:10 +0700

    chores: fix

--- client/src/components/Toogle.jsx
+++ client/src/components/Toogle.jsx
@@ -1,16 +1,16 @@
-import "./Toogle.css"
+// import "./Toogle.css"
 
-export const Toggle = ({ handleChange, isChecked }) => {
-  return (
-    <div className="toggle-container">
-      <input
-        type="checkbox"
-        id="check"
-        className="toggle"
-        onChange={handleChange}
-        checked={isChecked}
-      />
-      <label htmlFor="check">Dark Mode</label>
-    </div>
-  );
-};
+// export const Toggle = ({ handleChange, isChecked }) => {
+//   return (
+//     <div className="toggle-container">
+//       <input
+//         type="checkbox"
+//         id="check"
+//         className="toggle"
+//         onChange={handleChange}
+//         checked={isChecked}
+//       />
+//       <label htmlFor="check">Dark Mode</label>
+//     </div>
+//   );
+// };
